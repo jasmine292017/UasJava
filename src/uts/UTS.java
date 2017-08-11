4d90a64c343ade7e5818b0f11c6cff27a42ded13
Jasmine Nurul R <jasmine29797@gmail.com>
2017-08-10 19:19:50 -0700

    Tugas 4 Java

---
+++ src/uts/UTS.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts;
+
+import koneksi.konekdb;
+
+/**
+ *
+ * @author VAIO
+ */
+public class UTS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        konekdb out = new konekdb ();
+        out.setVisible(true);
+    }
+    
+}
